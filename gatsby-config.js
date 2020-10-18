c608253a40d486734e26ed0cab6ffe5950791dbd
DESKTOP-9E650IR\lee Stone <haiduc.dang91@gmail.com>
2020-10-18 18:11:47 +0200

    add g-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,3 +4,14 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-plugin-gtag`,
+        options: {
+          // replace `UA-XXXXXXXXX-X` with your own Google Analytics Tracking ID
+          trackingId: `UA-180766053-1`,
+        },
+      },
+    ],
+  }
